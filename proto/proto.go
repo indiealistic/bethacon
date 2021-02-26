ee4deac063cc26d6bc11ba619974954e3a2eb6a7
Indiealistic <hey@indiealistic.studio>
2021-02-26 21:22:42 +0700

    Fixed maybe

--- proto/proto.go
+++ proto/proto.go
@@ -5,4 +5,4 @@ package proto
 //go:generate protoc --proto_path=$GOPATH/src:. --go_out=$GOPATH/src $GOPATH/src/github.com/indiealistic/bethacon/proto/common/error_response.proto
 //go:generate protoc --proto_path=$GOPATH/src:. --go_out=$GOPATH/src $GOPATH/src/github.com/indiealistic/bethacon/proto/common/types.proto
 
-//go:generate protoc -I $GOPATH/src/github.com/prysmaticlabs/ethereumapis --proto_path=$GOPATH/src:. --go-grpc_out=$GOPATH/src --go_out=$GOPATH/src $GOPATH/src/github.com/indiealistic/bethacon/proto/bethacon/bethacon.proto
+//go:generate protoc --proto_path=$GOPATH/src:. --go-grpc_out=$GOPATH/src --go_out=$GOPATH/src $GOPATH/src/github.com/indiealistic/bethacon/proto/bethacon/bethacon.proto
